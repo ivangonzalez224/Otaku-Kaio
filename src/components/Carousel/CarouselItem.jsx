37ec2034371f02c05e0f919a39ce21bccc896c06
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-01-25 21:29:01 -0500

    Create the carousel item for offerts

---
+++ src/components/Carousel/CarouselItem.jsx
@@ -0,0 +1,10 @@
+const CarouselItem = ( ) => (
+  <div className="carousel-item">
+    <img
+      src= "https://res.cloudinary.com/dv1mouzbi/image/upload/v1706235080/devSources/Discurso_de_ventas_para_Deliexpre%CC%81s_m2eull.png"
+      alt= "Yuyu Hakusho offert"
+    />
+  </div>
+);
+
+export default CarouselItem;
