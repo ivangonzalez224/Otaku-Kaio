f76e61b8d432dd71663727452c724d41cafa098d
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-09 14:56:37 -0500

    Include the OfferLogic component in the home page

--- src/components/Offer/OfferLogic.jsx
+++ src/components/Offer/OfferLogic.jsx
@@ -5,8 +5,8 @@ const OfferLogic = () => {
     ];
   
     return (
-      <div className="grid grid-cols-3 gap-8">
-        <div className="col-span-1">
+      <div className="offerMain grid grid-cols-3 gap-8">
+        <div className="offerCallAction flex flex-col items-start">
           <h2 className="text-xl font-bold mb-2">Daily offer</h2>
           <p className="text-gray-600 mb-4">Check out our daily offers combined with big discounts on some products. Do not miss your chance.</p>
           <button className="inline-flex items-center px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700">
@@ -14,17 +14,17 @@ const OfferLogic = () => {
           </button>
         </div> 
         {offerOptions.map(option => (
-          <div key={option.id} className="col-span-1 relative rounded-lg overflow-hidden bg-gray-200">
+          <div key={option.id} className="offerImagesDiv col-span-1 w-80 mx-auto relative rounded-lg overflow-hidden bg-gray-200"> 
+            <div className="offerItemTop absolute flex justify-between bottom-0 left-0 right-0">
+                <h3 className="text-xl font-bold inline-block mr-2">{option.title}</h3>
+                <span className="text-gray-600">{option.discount}%</span>
+            </div>
             <img
               src={option.image}
               alt="Background image"
-              className="absolute inset-0 w-full h-full object-cover"
+              className="object-fit: contain"
             />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              <h3 className="text-xl font-bold inline-block mr-2">{option.title}</h3>
-              <span className="text-gray-600">{option.discount}%</span>
-            </div>
-          </div>
+         </div>
         ))}
       </div>
     );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import CarouselLogic from '../Carousel/CarouselLogic';
 import CategoryLogic from '../Categories/CategoryLogic';
+import OfferLogic from '../Offer/OfferLogic';
 
 const Home = () =>  (
   <div className="main_home flex flex-col justify-center w-100vw bg-gray-200">
     <CarouselLogic />
     <CategoryLogic />
+    <OfferLogic />
   </div>
   );
 
