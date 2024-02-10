cf23a5167dc21eaf09acf8c028e496a1f0314e04
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-10 09:52:37 -0500

    Add styles to the items in the OfferLogic Component

--- src/components/Offer/OfferLogic.jsx
+++ src/components/Offer/OfferLogic.jsx
@@ -5,30 +5,30 @@ const OfferLogic = () => {
     ];
   
     return (
-      <div className="mainContainer h-screen bg-gray-100 flex justify-center items-center">  
-        <div className="offerMain grid grid-cols-3 gap-8 bg-white rounded-md shadow-md w-full md:w-[80%]">
-            <div className="offerCallAction flex flex-col items-start">
+      <div className="mainContainer flex justify-center items-center mt-16 mb-16">
+        <div className="offerMain grid grid-cols-3 gap-8 rounded-md shadow-md w-full md:w-[72%]">
+          <div className="offerCallAction flex flex-col items-start">
             <h2 className="text-xl font-bold mb-2">Daily offer</h2>
             <p className="text-gray-600 mb-4 text-left">Check out our daily offers combined with big discounts on some products. Do not miss your chance.</p>
             <button className="inline-flex items-center px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700">
-                Open Store
+              Open Store
             </button>
-            </div> 
-            {offerOptions.map(option => (
-            <div key={option.id} className="offerImagesDiv col-span-1 w-80 mx-auto relative rounded-lg overflow-hidden bg-gray-200"> 
-                <div className="offerItemTop absolute flex justify-between bottom-0 left-0 right-0">
-                    <h3 className="text-xl font-bold inline-block mr-2">{option.title}</h3>
-                    <span className="text-gray-600">{option.discount}%</span>
-                </div>
-                <img
+          </div>
+          {offerOptions.map((option, index) => (
+            <div key={option.id} className={`offerImagesDiv col-span-1 w-80 mx-auto rounded-lg overflow-hidden flex flex-col ${index === 0 ? 'bg-blue-100' : 'bg-yellow-100'}`}>
+              <div className="offerItemTop flex justify-between items-center px-4 py-2 mb-2">
+                <h3 className="text-xl font-bold inline-block mr-2">{option.title}</h3>
+                <span className="text-gray-600 bg-white rounded-full py-3 px-2">{option.discount}%</span>
+              </div>
+              <img
                 src={option.image}
                 alt="Background image"
-                className="object-fit: contain"
-                />
+                className="w-full h-60 object-cover" // Set image width and height to cover the container
+              />
             </div>
-            ))}
+          ))}
         </div>
-      </div>  
+      </div>
     );
   };
   
