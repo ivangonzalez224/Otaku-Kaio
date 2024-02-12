2b805e0dc0297c29122980d843a4390ba5412452
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-11 22:24:19 -0500

    Update the styles to the Home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import CategoryLogic from '../Categories/CategoryLogic';
 import OfferLogic from '../Offer/OfferLogic';
 
 const Home = () =>  (
-  <div className="main_home flex flex-col justify-center w-100vw bg-gray-200">
+  <div className="main_home flex flex-col justify-center w-100vw bg-white mx-auto">
     <CarouselLogic />
     <CategoryLogic />
     <OfferLogic />
