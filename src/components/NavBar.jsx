1a3a41bce64ff185e7aa4f752e05e3be79c68bb0
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-12 15:19:36 -0500

    Update the styles in the nav bar using TailwindCSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,41 +2,40 @@ import { NavLink } from 'react-router-dom';
 import { FaRegHeart } from "react-icons/fa6";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
-import '../assets/styles/NavBar.css';
 
 const NavBar = () => (
-    <nav>
-      <ul>
-        <li id="nav_left">
-          <NavLink to="/"><img src='' alt="Logo" /></NavLink>
-        </li>
-      </ul>
-      <ul className="nav_center">
-        <li id="nav_options" className="nav-homeOpt">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li id="nav_options">
-          <NavLink to="/">About</NavLink>
-        </li>
-        <li id="nav_options">
-          <NavLink to="/">Store</NavLink>
-        </li>
-        <li id="nav_options">
-          <NavLink to="/">Offerts</NavLink>
-        </li>
-      </ul>
-      <ul id="nav-rigth">
-        <li>
-          <NavLink ><FaRegHeart id="heart-btn" /></NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart"><IoCartOutline /></NavLink>
-        </li>
-        <li id="nav-userIcon">
-          <NavLink to="/profile"><FaRegUser /></NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-  
-  export default NavBar;
+  <nav className="w-full px-4 flex items-center justify-between h-16 bg-white font-bold text-gray-700 sticky top-0 z-1 box-border">
+    <ul className="flex items-center">
+      <li className="flex items-center">
+        <NavLink to="/"><img src='' alt="Logo" className="w-10 h-10 mr-4" /></NavLink>
+      </li>
+    </ul>
+    <ul className="w-99 flex items-center justify-around bg-gray-200 px-2 py-1 rounded-lg">
+      <li className="rounded-lg px-4 py-2 bg-white font-bold">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="px-4 py-2">
+        <NavLink to="/">About</NavLink>
+      </li>
+      <li className="px-4 py-2">
+        <NavLink to="/">Store</NavLink>
+      </li>
+      <li className="px-4 py-2">
+        <NavLink to="/">Offerts</NavLink>
+      </li>
+    </ul>
+    <ul className="flex items-center justify-end">
+      <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
+        <NavLink to="/"><FaRegHeart /></NavLink>
+      </li>
+      <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
+        <NavLink to="/cart"><IoCartOutline /></NavLink>
+      </li>
+      <li className="flex items-center justify-center rounded-full bg-yellow-400 px-4 py-3">
+        <NavLink to="/profile"><FaRegUser /></NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
