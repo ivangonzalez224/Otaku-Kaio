3e2a9bc2bddb9120238ab279f309d05e91f638a7
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-18 21:43:32 -0500

    Add the footer component into the home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import CarouselLogic from '../Carousel/CarouselLogic';
 import CategoryLogic from '../Categories/CategoryLogic';
 import OfferLogic from '../Offer/OfferLogic';
 import ArrivalsLogic from '../Arrivals/ArrivalsLogic';
+import FooterLogic from '../Footer/FooterLogic';
 
 const Home = () =>  (
   <div className="main_home flex flex-col justify-center w-100vw bg-white mx-auto">
@@ -9,6 +10,7 @@ const Home = () =>  (
     <CategoryLogic />
     <OfferLogic />
     <ArrivalsLogic />
+    <FooterLogic />
   </div>
   );
 
