6fe0c82195de441acdca0b3eee891e04516b32d7
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-02-24 13:50:50 -0500

    Create the Store page main structure

---
+++ src/components/pages/Produtcs.jsx
@@ -0,0 +1,9 @@
+import ProductslLogic from '../Products/ProductsLogic';
+
+const Products = () =>  (
+  <div className="main_products flex flex-col justify-center w-100vw bg-white mx-auto">
+    <ProductslLogic /> 
+  </div>
+  );
+
+export default Products;
