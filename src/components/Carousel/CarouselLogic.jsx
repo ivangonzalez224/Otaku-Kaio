b6355fcdf1ae2aae57735c60dd49e8fba10375a9
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-03-18 13:36:51 -0500

    Update the Carousel logic component

--- src/components/Carousel/CarouselLogic.jsx
+++ src/components/Carousel/CarouselLogic.jsx
@@ -1,13 +1,33 @@
 import CarouselItem from './CarouselItem';
-import { Carousel } from "@material-tailwind/react";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const CarouselLogic = () => {
     const carouselItems = [{id:1, image: 'https://res.cloudinary.com/dv1mouzbi/image/upload/v1709336758/devSources/carouselAnime1_syz7dy.png'}];
-    
+    const responsive = {
+      superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5,
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 992 },
+        items: 3,
+      },
+      tablet: {
+        breakpoint: { max: 992, min: 765 },
+        items: 2,
+      },
+      mobile: {
+        breakpoint: { max: 765, min: 0 },
+        items: 1,
+      },
+    };
     return (
-      <Carousel className="rounded-xl">
+      <Carousel responsive={responsive}>
         {carouselItems.map((item) => (
-          <CarouselItem key={item.id} itemImage={item.image} />
+          <CarouselItem
+            key={item.id}
+            itemImage={item.image} />
         ))}
       </Carousel>
     );
