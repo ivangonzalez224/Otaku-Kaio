e4ed4c31386aa4171cd8c7e918c42d2bea76b0d4
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-05 08:06:26 -0500

    Include react icons in the store iten component

--- src/components/Store/StoreItem.jsx
+++ src/components/Store/StoreItem.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { TbShoppingCartFilled } from "react-icons/tb";
 
 const StoreItem = ({ image, name, originalPrice, salePrice }) => {
   return (
     <div className="flex flex-col justify-end rounded-lg bg-white border border-blue-500 p-2">
       <img src={image} alt={name} className="mx-auto" />
-      <h3 className="font-bold text-left mb-2">{name}</h3>
-      <div className="flex justify-between text-sm">
-        <span className="text-gray-500 line-through">$ {originalPrice}</span>
-        <span className="text-red-500 font-bold">$ {salePrice}</span>
+      <h3 className="text-left my-2">{name}</h3>
+      <div className="flex justify-start text-sm">
+        <span className="text-gray-500 line-through mr-6">$ {originalPrice}</span>
+        <span className="text-red-500 font-bold text-[17px]">$ {salePrice}</span>
       </div>
       <div className="flex justify-between items-center mt-4">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+          className="bg-blue-500 hover:bg-blue-700 text-[15px] focus:outline-none text-white  py-2 px-4 rounded-md"
           // onClick={onDetailsClick}
         >
           Details
         </button>
         <button
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-md ml-4"
+          className="bg-yellow-500 hover:bg-yellow-700 focus:outline-none text-white font-bold py-2 px-4 rounded-md ml-4"
           // onClick={onCartClick}
         >
-          <svg className="fill-current h-6 w-6" viewBox="0 0 24 24">
-          </svg>
+          <TbShoppingCartFilled />
         </button>
       </div>
     </div>
