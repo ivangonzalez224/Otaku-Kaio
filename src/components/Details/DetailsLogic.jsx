fd28b178ec9c5b6b3fd0eed648273d65a57e8d50
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-07 13:57:05 -0500

    Fetch and filter the product selected to show the details

--- src/components/Details/DetailsLogic.jsx
+++ src/components/Details/DetailsLogic.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import DetailsItem from './DetailsItem';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { getProducts } from '../../redux/products/ProductsSlice';
 
 const DetailsLogic = () => {
-  const productId = useSelector((state) => state.params.productId);
   const dispatch = useDispatch();
+  const { productId } = useParams();
+  const { productItems } = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(getProducts(productId));
-  }, [productId, dispatch]);
+    if (productItems.length === 0) {
+      dispatch(getProducts());  
+    }
+  });
 
-  const product = useSelector((state) => state.products.byId[productId]);
-
+  const filteredItem = productItems.filter((product) => product.id == productId);
   return (
     <div>
-      {product && <DetailsItem product={product} />}
-      {!product && <p>Loading product details...</p>}
+      {filteredItem && <DetailsItem product={filteredItem} />}
+      {!filteredItem && <p>Loading product details...</p>}
     </div>
   );
 };
