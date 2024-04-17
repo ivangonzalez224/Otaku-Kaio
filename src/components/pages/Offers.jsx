19da351662f87ba7f4cb54a0cf608f2226a497aa
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-17 10:15:50 -0500

    Create the offers page main structure

---
+++ src/components/pages/Offers.jsx
@@ -0,0 +1,10 @@
+import FooterLogic from '../Footer/FooterLogic';
+
+const Offers = () =>  (
+  <div className="main_offers flex flex-col items-center justify-center w-100vw bg-gray mx-auto py-2 box-border">
+    <h3>Offers<h3 />
+    <FooterLogic />
+  </div>
+  );
+
+export default Offers;
