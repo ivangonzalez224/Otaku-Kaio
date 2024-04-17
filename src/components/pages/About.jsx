84a44c063ea57253eff5644cfc2c5f7898e44971
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-17 10:14:11 -0500

    Create the about page main structure

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+import FooterLogic from '../Footer/FooterLogic';
+
+const About = () =>  (
+  <div className="main_aboout flex flex-col items-center justify-center w-100vw bg-gray mx-auto py-2 box-border">
+    <h3>About<h3 />
+    <FooterLogic />
+  </div>
+  );
+
+export default About;
