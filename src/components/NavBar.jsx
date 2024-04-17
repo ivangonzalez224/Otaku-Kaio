2337268444e7819130e264845ceee8feeb46d2f4
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-17 10:55:14 -0500

    Add styles for the active options in the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,58 @@
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaRegHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
 
-const NavBar = () => (
-  <nav className="w-full px-4 flex items-center justify-between h-16 bg-white font-bold text-gray-700 sticky top-0 z-1 box-border">
-    <ul className="flex items-center">
-      <li className="flex items-center">
-        <NavLink to="/"><img src='' alt="Logo" className="w-10 h-10 mr-4" /></NavLink>
-      </li>
-    </ul>
-    <ul className="w-99 flex items-center justify-around bg-gray-200 px-2 py-1 rounded-lg">
-      <li className="rounded-lg px-4 py-2 bg-white font-bold">
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li className="px-4 py-2">
-        <NavLink to="/">About</NavLink>
-      </li>
-      <li className="px-4 py-2">
-        <NavLink to="/store">Store</NavLink>
-      </li>
-      <li className="px-4 py-2">
-        <NavLink to="/">Offerts</NavLink>
-      </li>
-    </ul>
-    <ul className="flex items-center justify-end">
-      <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
-        <NavLink to="/"><FaRegHeart /></NavLink>
-      </li>
-      <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
-        <NavLink to="/cart"><IoCartOutline /></NavLink>
-      </li>
-      <li className="flex items-center justify-center rounded-full bg-yellow-400 px-4 py-3">
-        <NavLink to="/profile"><FaRegUser /></NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("/");
+
+  const handleNavLinkClick = (to) => {
+    setActiveLink(to);
+  };
+
+  return (
+    <nav className="w-full px-4 flex items-center justify-between h-16 bg-white font-bold text-gray-700 sticky top-0 z-1 box-border">
+      <ul className="flex items-center">
+        <li className="flex items-center">
+          <NavLink to="/">
+            <img src='' alt="Logo" className="w-10 h-10 mr-4" />
+          </NavLink>
+        </li>
+      </ul>
+      <ul className="w-99 flex items-center justify-around bg-gray-200 px-2 py-1 rounded-lg">
+        <li className={`rounded-lg px-4 py-2 hover:bg-gray-300 cursor-pointer ${activeLink === "/" ? 'bg-white' : ''}`} onClick={() => handleNavLinkClick("/")}>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className={`rounded-lg px-4 py-2 hover:bg-gray-300 cursor-pointer ${activeLink === "/about" ? 'bg-white' : ''}`} onClick={() => handleNavLinkClick("/about")}>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li className={`rounded-lg px-4 py-2 hover:bg-gray-300 cursor-pointer ${activeLink === "/store" ? 'bg-white' : ''}`} onClick={() => handleNavLinkClick("/store")}>
+          <NavLink to="/store">Store</NavLink>
+        </li>
+        <li className={`rounded-lg px-4 py-2 hover:bg-gray-300 cursor-pointer ${activeLink === "/offers" ? 'bg-white' : ''}`} onClick={() => handleNavLinkClick("/offers")}>
+          <NavLink to="/offers">Offers</NavLink>
+        </li>
+      </ul>
+      <ul className="flex items-center justify-end">
+        <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
+          <NavLink to="/">
+            <FaRegHeart />
+          </NavLink>
+        </li>
+        <li className="mr-4 flex items-center justify-center rounded-full border border-gray-700 px-4 py-3">
+          <NavLink to="/cart">
+            <IoCartOutline />
+          </NavLink>
+        </li>
+        <li className="flex items-center justify-center rounded-full bg-yellow-400 px-4 py-3">
+          <NavLink to="/profile">
+            <FaRegUser />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
