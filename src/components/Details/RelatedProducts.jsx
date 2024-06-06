158a415e3c31e653115fc9c294691d2e8550d6ad
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-06-06 07:54:05 -0500

    Update styles in the related products carousel

--- src/components/Details/RelatedProducts.jsx
+++ src/components/Details/RelatedProducts.jsx
@@ -3,6 +3,7 @@ import RelatedItem from './RelatedItem';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import '../../assets/styles/carousel.css';
+import '../../assets/styles/relatedCarousel.css';
 
 const RelatedProducts = ({ products }) => {
   
@@ -12,32 +13,39 @@ const RelatedProducts = ({ products }) => {
       items: 5,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 992 },
+      breakpoint: { max: 3000, min: 1160 },
       items: 4,
     },
     tablet: {
-      breakpoint: { max: 992, min: 765 },
+      breakpoint: { max: 1160, min: 745 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 745, min: 665 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 765, min: 0 },
+      breakpoint: { max: 665, min: 0 },
       items: 1,
     },
   };
   return (
-    <div className="w-5/6 flex flex-col items-start justify-center border-t border-gray-200 box-border mt-20 pt-6 px-10">
+    <div className="w-5/6 flex flex-col justify-center border-t border-gray-200 box-border mt-20 pt-6 px-4">
       <h6 className="text-left text-gray-600">CUSTOMERS WHO VIEWED THIS PRODUCT ALSO VIEWED</h6>
-      <Carousel responsive={responsive}>
+      <Carousel responsive={responsive} className="custom-carousel">
         {products.map((product) => (
-          <RelatedItem
-            key={product.id}
-            name={product.name}
-            image={product.image}
-            priceList={product.ori_price}
-            sellingPrice={product.sale_price}
-            className="mr-10 box-border"
-          />
-        ))}
+            <div key={product.id} className="mx-8">
+              <RelatedItem
+                key={product.id}
+                name={product.name}
+                image={product.image}
+                priceList={product.ori_price}
+                sellingPrice={product.sale_price}
+                className="mr-10 box-border"
+              />
+            </div>  
+          ),
+        )}
       </Carousel>  
     </div>
   );
