6049b212f274a4e9d6e395483a74b9f22512d645
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-06-07 08:44:04 -0500

    Create loginUser function to login with the demo user when the app starts

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,16 @@ import PrivacyPolicy from './components/pages/PrivacyPolicy';
 import store from './redux/store'
 import './App.css';
 import { getLandings } from './redux/landings/LandingsSlice';
-import { generateRandomString } from './utils';
+import { loginUser } from './utils';
 
 store.dispatch(getLandings());
 
-// Create a new user
-async function createUser() {
-  const num = Math.floor(Math.random() * 1000); // Generate a random number for user name and email
-  const userData = {
-    name: `user${num}`,
-    email: `user${num}@example.com`,
-    password: generateRandomString(8)
-  };
-
-  try {
-    const response = await fetch('http://127.0.0.1:3000/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to create user');
-    }
-
-    const data = await response.json();
-    console.log('User created successfully:', data); // Log user data
-  } catch (error) {
-    console.error('Error creating user:', error);
-  }
-}
-
 const App = () => {
   useEffect(() => {
-    //createUser();
-  }, []);
+    const demoEmail = 'demo_user@example.com';
+    const demoPassword = 'password123';
+    loginUser(demoEmail, demoPassword);
+  }, [];
   
   return (
     <div className="main-container">
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,22 @@
-export function generateRandomString(length) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+export const loginUser = async (email, password) => {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to log in');
+    }
+
+    const data = await response.json();
+    console.log('User logged in successfully:', data);
+    // Store the JWT token
+    localStorage.setItem('token', data.token);
+  } catch (error) {
+    console.error('Error logging in:', error);
   }
-  return result;
-}
+};
