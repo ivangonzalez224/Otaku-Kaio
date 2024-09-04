4507f535cd05398dae863bb92400512cca17e4d3
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-04 14:07:27 -0500

    Update the styles in the offers section in the home page for mobile versions

--- src/components/Offer/OfferLogic.jsx
+++ src/components/Offer/OfferLogic.jsx
@@ -21,7 +21,7 @@ const OfferLogic = () => {
 
     return (
       <div className="mainContainer flex justify-center items-center mt-16 mb-16">
-        <div className="offerMain grid grid-cols-3 gap-8 w-full md:w-[82%]">
+        <div className="offerMain grid grid-cols-1 md:grid-cols-3 gap-8 w-[85%] max-w-[768px] mx-auto">
           <div className="offerCallAction flex flex-col items-start">
             <h2 className="text-xl font-bold mb-2">Daily offer</h2>
             <p className="text-gray-600 mb-4 text-left">Check out our daily offers combined with big discounts on some products. Do not miss your chance.</p>
@@ -32,9 +32,13 @@ const OfferLogic = () => {
             </button>
           </div>
           {offerOptions.map((option, index) => (
-            <div key={option.id} className={`offerImagesDiv col-span-1 w-80 mx-auto rounded-lg overflow-hidden flex flex-col items-center justify-center ${index === 0 ? 'bg-blue-100' : 'bg-yellow-100'}`}>
+            <div
+              key={option.id}
+              className={`offerImagesDiv w-full rounded-lg overflow-hidden flex flex-col items-center justify-center ${index === 0 ? 'bg-blue-100' : 'bg-yellow-100'
+              }`}
+            >
               <div className="offerItemTop w-full flex justify-between items-center px-4 pt-2 mb-0">
-                <h3 className="text-xl font-bold inline-block mr-2">{option.title}</h3>
+                <h3 className="text-lg md:text-xl font-bold inline-block mr-2">{option.title}</h3>
                 <span className="text-gray-600 bg-white rounded-full py-1 px-2 flex flex-col items-center text-center">
                   <span className="text-base font-bold">{Math.floor(option.offer)}%</span>
                   <span className="text-xs">OFF</span>
