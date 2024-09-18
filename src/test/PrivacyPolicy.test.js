6b64008c54c04732fec1bd2f46a2a63f4b10b223
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-18 12:43:01 -0500

    Add a test to verify if the main imge in the Privacy Policy page renders correctly

--- src/test/PrivacyPolicy.test.js
+++ src/test/PrivacyPolicy.test.js
@@ -16,4 +16,23 @@ describe('PrivacyPolicy Component', () => {
     const titleElement = screen.getByRole('heading', { level: 1 });
     expect(titleElement).toHaveTextContent('Privacy Policy');
   });
+
+  test('renders image with correct src and alt text', () => {
+    render(<PrivacyPolicy />);
+    const imageElement = screen.getByAltText('Privacy P.');
+    expect(imageElement).toHaveAttribute('src', 'https://res.cloudinary.com/dv1mouzbi/image/upload/v1717606385/devSources/privacyPolicyTop_sc5vvf.png');
+  });
+
+  test('renders all the key sections', () => {
+    render(<PrivacyPolicy />);
+    
+    expect(screen.getByText(/PERSONAL INFORMATION WE COLLECT/i)).toBeInTheDocument();
+    expect(screen.getByText(/HOW DO WE USE YOUR PERSONAL INFORMATION/i)).toBeInTheDocument();
+    expect(screen.getByText(/SHARING YOUR PERSONAL INFORMATION/i)).toBeInTheDocument();
+    expect(screen.getByText(/YOUR RIGHTS/i)).toBeInTheDocument();
+    expect(screen.getByText(/DATA RETENTION/i)).toBeInTheDocument();
+    expect(screen.getByText(/MINORS/i)).toBeInTheDocument();
+    expect(screen.getByText(/CHANGES/i)).toBeInTheDocument();
+    expect(screen.getByText(/CONTACT US/i)).toBeInTheDocument();
+  });
 });
